4582b8bccbae0e7fd96d14aaa5c3bc8eba71a17c
Madison <madisonkanna@gmail.com>
2024-08-01 08:45:43 -0700

    write

--- app/main.js
+++ app/main.js
@@ -7,6 +7,10 @@ const server = net.createServer((socket) => {
 	socket.on("close", () => {
 		socket.end();
 	});
+
+	socket.write('HTTP/1.1 200 OK\r\n\r\n');
+
 });
 
 server.listen(4221, "localhost");
+
